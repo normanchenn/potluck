584c7cdd061cde0364fa5efbe8dc52eb94cc9e0a
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2023-09-16 18:51:58 -0400

    get user ingredients by id and update ingredients

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -8,28 +8,35 @@ import {
 
 export const router = createTRPCRouter({
   getUser: publicProcedure.query(({ ctx }) => {
-      return ctx.session?.user;
-    }),
+    return ctx.session?.user;
+  }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.example.findMany();
+    return ctx.db.user.findMany();
   }),
 
+  // returns the current user id's (or, optionally, the passed user id's) ingredients
   getIngredients: protectedProcedure
-  .query(({ ctx }) => {
-    return ctx.db.user.findUnique({
-      where: {
-        id: ctx.session.user.id
-      }
-    })
-  }),
+    .input(z.object({ id: z.optional(z.string()) }))
+    .query(({ ctx, input: { id } }) => {
+      return ctx.db.user.findUnique({
+        where: {
+          id: id ?? ctx.session.user.id
+        }
+      })
+    }),
 
   updateIngredients: protectedProcedure
-  .query(({ ctx }) => {
-    return ctx.db.user.findUnique({
-      where: {
-        id: ctx.session.user.id
-      }
-    })
-  }),
+    .input(z.object({ ingredients: z.string() }))
+    .query(({ ctx, input: { ingredients } }) => {
+      ctx.db.user.update
+      return ctx.db.user.update({
+        where: {
+          id: ctx.session.user.id
+        },
+        data: {
+          ingredients
+        }
+      })
+    }),
 });
