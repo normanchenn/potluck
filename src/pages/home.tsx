3d043c9c59d4982183755a1d93d1b5599d884a1d
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2023-09-16 18:34:27 -0400

    initial ingredients crud

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,21 @@
+import { useSession } from "next-auth/react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+
+import { api } from "~/utils/api";
+
 export default function home() {
-  return <h1>Hello this is a page</h1>;
+  const session = useSession();
+  // const user = api.example.getIngredients.useQuery({ id: session.data?.user.id });
+  const user = api.example.getIngredients.useQuery();
+  console.log("USER", user.data);
+
+  // console.log(session.data?.user.id)
+  const myFunc = () => {
+    console.log("hi there");
+  }
+  return <>
+    <Input />
+    <Button onClick={myFunc}>My Button</Button>
+  </>;
 }
