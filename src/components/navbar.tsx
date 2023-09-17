63dbea8870c97add62ffe1e1433d2121ea9adf7c
Jonathan Zhou <jonathanzhou8@gmail.com>
2023-09-16 20:27:35 -0400

    navbar working, no working frontend auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <Link href="/">
+    <nav className="navbar flex justify-between">
+      <Link href="/" className="ml-1">
         <Image
           src="/logo.png"
-          width={150}
-          height={150}
+          width={50}
+          height={50}
           alt="Picture of the author"
         />
       </Link>
-      <Link href="/account">
-        <Image
-          src="/logo.png"
-          width={150}
-          height={150}
-          alt="Picture of the author"
-        />{" "}
-      </Link>
+      <Avatar className="mr-5 mt-1">
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </nav>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,44 @@ import { Button } from "~/components/ui/button";
 import logo from "public/logo.png";
 import Image from "next/image";
 import { api } from "~/utils/api";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+
+  const customSignIn = async () => {
+    await signIn(); // Perform sign in
+
+    if (sessionData) {
+      router.push("/home"); // Redirect to /home if signed in
+    }
+  };
+
+  function AuthShowcase() {
+    const { data: sessionData } = useSession();
+
+    const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+      undefined, // no input
+      { enabled: sessionData?.user !== undefined },
+    );
+
+    return (
+      <div className="flex flex-col items-center justify-center gap-4">
+        <p className="text-center text-2xl text-white">
+          {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+          {secretMessage && <span> - {secretMessage}</span>}
+        </p>
+        <button
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          onClick={sessionData ? () => void signOut() : customSignIn} // Use customSignIn function
+        >
+          {sessionData ? "Sign out" : "Sign in"}
+        </button>
+      </div>
+    );
+  }
+
   return (
     <>
       <Head>
@@ -33,29 +69,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
   );
 }
